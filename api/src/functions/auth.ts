8f93348b4e310b019d5c81962722ce732111343f
Mark Wiemer <markwiemer@outlook.com>
2024-10-24 21:11:52 -0700

    Shutdown signups

--- api/src/functions/auth.ts
+++ api/src/functions/auth.ts
@@ -125,20 +125,8 @@ export const handler = async (
     //
     // If this returns anything else, it will be returned by the
     // `signUp()` function in the form of: `{ message: 'String here' }`.
-    handler: ({
-      username,
-      hashedPassword,
-      salt,
-      userAttributes: _userAttributes,
-    }) => {
-      return db.user.create({
-        data: {
-          email: username,
-          hashedPassword: hashedPassword,
-          salt: salt,
-          // name: userAttributes.name
-        },
-      })
+    handler: () => {
+      return false
     },
 
     // Include any format checks for password here. Return `true` if the
--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -9,7 +9,6 @@ const Routes = () => {
   return (
     <Router useAuth={useAuth}>
       <Route path="/login" page={LoginPage} name="login" />
-      <Route path="/signup" page={SignupPage} name="signup" />
       <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
       <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
       <PrivateSet unauthenticated="home">
