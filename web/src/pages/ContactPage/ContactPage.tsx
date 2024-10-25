fbf53e70379ffa0ecf5b5a6be93712c9b62ff849
Mark Wiemer <markwiemer@outlook.com>
2024-10-24 20:23:52 -0700

    Clear form on success

--- web/src/pages/ContactPage/ContactPage.tsx
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -12,6 +12,7 @@ import {
   FieldError,
   Label,
   FormError,
+  useForm,
 } from '@redwoodjs/forms'
 import { Metadata, useMutation } from '@redwoodjs/web'
 import { toast, Toaster } from '@redwoodjs/web/toast'
@@ -31,12 +32,15 @@ const CREATE_CONTACT = gql`
 `
 
 const ContactPage = () => {
+  const formMethods = useForm()
+
   const [create, { loading, error }] = useMutation<
     CreateContactMutation,
     CreateContactMutationVariables
   >(CREATE_CONTACT, {
     onCompleted: () => {
       toast.success('Thank you for your submission!')
+      formMethods.reset()
     },
   })
 
@@ -48,7 +52,12 @@ const ContactPage = () => {
     <>
       <Metadata title="Contact" description="Contact page" />
       <Toaster />
-      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }} error={error}>
+      <Form
+        onSubmit={onSubmit}
+        config={{ mode: 'onBlur' }}
+        error={error}
+        formMethods={formMethods}
+      >
         <FormError error={error} wrapperClassName="form-error" />
         {/* Name */}
         <Label name="name" errorClassName="error">
